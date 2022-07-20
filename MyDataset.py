893109b44c1d08618e8794db387e8e03be0d1325
abcrohit <66174896+abcrohit@users.noreply.github.com>
2022-07-20 13:07:22 +0530

    Update MyDataset.py

--- MyDataset.py
+++ MyDataset.py
@@ -102,12 +102,12 @@ class MyDatasetCompress(Dataset):
 
   def __getitem__(self,idx):
     return self.x_train[idx],self.y_train[idx]
- class datasetfromtensor(Dataset):
-    def __init__(self,X,y):
-        self.x_train=X
-        self.y_train=y
-    def __len__(self):
-        return len(self.y_train)
-    def __getitem__(self,idx):
-        return self.x_train[idx],self.y_train[idx]
+class datasetfromtensor(Dataset):
+  def __init__(self,X,y):
+     self.x_train=X
+     self.y_train=y
+  def __len__(self)
+     return len(self.y_train)
+  def __getitem__(self,idx):
+     return self.x_train[idx],self.y_train[idx]
     
